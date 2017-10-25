906894f3ef915c0901148ee204476212baedc466
Andrew Tritt <ajtritt@lbl.gov>
2017-10-24 20:42:15 -0700

    moved form to pynwb/form

---
+++ src/pynwb/form/build/__init__.py
@@ -0,0 +1,9 @@
+from .builders import Builder
+from .builders import GroupBuilder
+from .builders import DatasetBuilder
+from .builders import RegionBuilder
+from .builders import LinkBuilder
+
+from .map import ObjectMapper
+from .map import BuildManager
+from .map import TypeMap
