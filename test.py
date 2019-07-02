4365eedc3dca8522929b45e32b6a37de7f1e27ba
Ryan Ly <rly@lbl.gov>
2019-07-02 11:17:59 -0700

    Improve unit test runner output to make debugging easier (#89)
    * Improve unit test runner output to make debugging easier

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ import os
 import sys
 import traceback
 import unittest2 as unittest
+from tests.coloredtestrunner import ColoredTestRunner, ColoredTestResult
 
 flags = {'hdmf': 1, 'integration': 3, 'example': 4}
 
@@ -19,35 +20,14 @@ FAILURES = 0
 ERRORS = 0
 
 
-class SuccessRecordingResult(unittest.TextTestResult):
-    '''A unittest test result class that stores successful test cases as well
-    as failures and skips.
-    '''
-
-    def addSuccess(self, test):
-        if not hasattr(self, 'successes'):
-            self.successes = [test]
-        else:
-            self.successes.append(test)
-
-    def get_all_cases_run(self):
-        '''Return a list of each test case which failed or succeeded
-        '''
-
-        cases = []
-
-        if hasattr(self, 'successes'):
-            cases.extend(self.successes)
-        cases.extend([failure[0] for failure in self.failures])
-
-        return cases
-
-
 def run_test_suite(directory, description="", verbose=True):
     global TOTAL, FAILURES, ERRORS
     logging.info("running %s" % description)
     directory = os.path.join(os.path.dirname(__file__), directory)
-    runner = unittest.TextTestRunner(verbosity=verbose, resultclass=SuccessRecordingResult)
+    if verbose > 1:
+        runner = ColoredTestRunner(verbosity=verbose)
+    else:
+        runner = unittest.TextTestRunner(verbosity=verbose, resultclass=ColoredTestResult)
     test_result = runner.run(unittest.TestLoader().discover(directory))
 
     TOTAL += test_result.testsRun
