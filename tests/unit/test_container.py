95addecd9517a5c5d4cdca825fdc827c83777f9c
Ryan Ly <rly@lbl.gov>
2019-07-25 18:46:24 -0700

    Set object ID on get_object_id if not set

--- tests/unit/test_container.py
+++ tests/unit/test_container.py
@@ -20,11 +20,11 @@ class TestContainer(unittest.TestCase):
         self.assertIsNotNone(child_obj.object_id)
 
     def test_constructor_object_id_none(self):
-        """Test that setting object_id to None in __new__ is OK
+        """Test that setting object_id to None in __new__ is OK and the object ID is set on get
         """
         parent_obj = Container('obj1')
         child_obj = Container.__new__(Container, parent=parent_obj, object_id=None)
-        self.assertIsNone(child_obj.object_id)
+        self.assertIsNotNone(child_obj.object_id)
 
     def test_set_parent(self):
         """Test that parent setter properly sets parent
