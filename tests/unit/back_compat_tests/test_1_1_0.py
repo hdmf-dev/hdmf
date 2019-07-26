199dcc42564b595ba789a0a2a83a9b4108c690c6
Ryan Ly <rly@lbl.gov>
2019-07-25 18:48:49 -0700

    Add basic backwards compatibility test for reading 1.0.5 files

---
+++ tests/unit/back_compat_tests/test_1_1_0.py
@@ -0,0 +1,51 @@
+import unittest2 as unittest
+from shutil import copyfile
+import os
+
+from hdmf.backends.hdf5.h5tools import HDF5IO
+from tests.unit.test_io_hdf5_h5tools import _get_manager
+from tests.unit.test_utils import Foo, FooBucket
+
+
+class Test1_1_0(unittest.TestCase):
+
+    def setUp(self):
+        # created using manager in test_io_hdf5_h5tools
+        self.orig_1_0_5 = 'tests/unit/back_compat_tests/1.0.5.h5'
+        self.path_1_0_5 = 'test_1.0.5.h5'
+        copyfile(self.orig_1_0_5, self.path_1_0_5)
+
+        # note: this may break if the current manager is different from the old manager
+        # better to save a spec file
+        self.manager = _get_manager()
+
+    def tearDown(self):
+        if os.path.exists(self.path_1_0_5):
+            os.remove(self.path_1_0_5)
+
+    def test_read_1_0_5(self):
+        '''Test whether we can read files made by hdmf version 1.0.5'''
+        with HDF5IO(self.path_1_0_5, manager=self.manager, mode='r') as io:
+            read_foofile = io.read()
+            self.assertTrue(len(read_foofile.buckets) == 1)
+            self.assertListEqual(read_foofile.buckets[0].foos[0].my_data[:].tolist(), [0, 1, 2, 3, 4])
+
+    def test_append_1_0_5(self):
+        '''Test whether we can append to files made by hdmf version 1.0.5'''
+        foo = Foo('foo3', [10, 20, 30, 40, 50], "I am foo3", 17, 3.14)
+        foobucket = FooBucket('foobucket2', [foo])
+
+        with HDF5IO(self.path_1_0_5, manager=self.manager, mode='a') as io:
+            read_foofile = io.read()
+            read_foofile.buckets.append(foobucket)
+            foobucket.parent = read_foofile  # this should be done automatically
+            read_foofile.set_modified(True)  # this should be done automatically
+            io.write(read_foofile)
+
+        with HDF5IO(self.path_1_0_5, manager=self.manager, mode='r') as io:
+            read_foofile = io.read()
+            # workaround for the fact that appending to buckets changes order because groups are a dictionary behind
+            # the scenes
+            for bucket in read_foofile.buckets:
+                if bucket.name == 'foobucket2':
+                    self.assertListEqual(bucket.foos[0].my_data[:].tolist(), foo.my_data)
