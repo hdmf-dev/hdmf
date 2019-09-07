9d83a49913ad2d80b0222ee29f9e95e27252f6a0
Andrew Tritt <ajtritt@lbl.gov>
2019-09-06 17:05:47 -0700

    fix import error

--- tests/unit/common/test_table.py
+++ tests/unit/common/test_table.py
@@ -5,6 +5,7 @@ from hdmf.common import DynamicTable, VectorData, ElementIdentifiers, DynamicTab
 from . import base
 
 import pandas as pd
+import numpy as np
 
 
 class TestDynamicTable(unittest.TestCase):
