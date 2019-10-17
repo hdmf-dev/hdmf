cb86bf23b2818f26566671d6cc645ec0f709fc3b
Ryan Ly <rly@lbl.gov>
2019-10-16 18:31:48 -0700

    Remove print statements, fix #174

--- src/hdmf/spec/write.py
+++ src/hdmf/spec/write.py
@@ -244,18 +244,7 @@ def export_spec(ns_builder, new_data_types, output_dir):
     ns_path = ns_builder.name + '.namespace.yaml'
     ext_path = ns_builder.name + '.extensions.yaml'
 
-    if len(new_data_types) > 1:
-        pluralize = 's'
-    else:
-        pluralize = ''
-
-    print('Creating file {output_dir}/{ext_path} with {new_data_types_count} data type{pluralize}'.format(
-        pluralize=pluralize, output_dir=output_dir, ext_path=ext_path,
-        new_data_types_count=len(new_data_types)))
-
     for data_type in new_data_types:
         ns_builder.add_spec(ext_path, data_type)
-
-    print('Creating file {output_dir}/{ns_path}'.format(output_dir=output_dir, ns_path=ns_path))
 
     ns_builder.export(ns_path, outdir=output_dir)
