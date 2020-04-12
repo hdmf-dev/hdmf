337d26e7c87ee80c7a72c571db2dc8fda39e3a72
Ben Dichter <ben.dichter@gmail.com>
2020-04-12 10:48:40 -0400

    Update h5_utils.py

--- src/hdmf/backends/hdf5/h5_utils.py
+++ src/hdmf/backends/hdf5/h5_utils.py
@@ -428,7 +428,7 @@ class H5DataIO(DataIO):
         if not self.filter_available(self.__iosettings.get('compression', None),
                                      self.__allow_plugin_filters):
             msg = "%s compression may not be supported by this version of h5py." % str(self.__iosettings['compression'])
-            if not allow_plugin_filter:
+            if not self.__allow_plugin_filters:
                 msg += "Set `allow_plugin_filters=True` to enable the use of dynamically-loaded plugin filters."
             raise ValueError(msg)
         # Check possible parameter collisions
