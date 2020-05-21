f38d353786b38d95a1a7bb5f7b1f7f2d8f9bf547
Andrew Tritt <ajtritt@lbl.gov>
2020-05-20 17:56:25 -0700

    fix errors

--- tests/unit/common/test_table.py
+++ tests/unit/common/test_table.py
@@ -1,3 +1,4 @@
+import unittest
 from hdmf.common import DynamicTable, VectorData, VectorIndex, ElementIdentifiers, DynamicTableRegion, VocabData
 from hdmf.testing import TestCase, H5RoundTripMixin
 
@@ -546,6 +547,7 @@ class TestDynamicTableRegion(TestCase):
         except AttributeError:
             self.fail("DynamicTableRegion table setter raised AttributeError unexpectedly!")
 
+    @unittest.skip('we no longer check data contents for performance reasons')
     def test_dynamic_table_region_set_with_bad_data(self):
         table = self.with_columns_and_data()
         dynamic_table_region = DynamicTableRegion('dtr', [5, 1], 'desc')   # index 5 is out of range
