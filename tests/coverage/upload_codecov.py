c87edb92a280d79fbee60f1b98b28273fd735183
Ryan Ly <rly@lbl.gov>
2020-07-08 18:11:22 -0700

    Try 5 map azure codecov token

--- tests/coverage/upload_codecov.py
+++ tests/coverage/upload_codecov.py
@@ -2,6 +2,7 @@
 
 from subprocess import call
 import time
+import os
 
 if __name__ == '__main__':
     max_tries = 10
@@ -11,6 +12,7 @@ if __name__ == '__main__':
     while rc != 0 and num_tries < max_tries:
         time.sleep(5)
         rc = call(cmd, shell=True)
+        os.remove(os.path.join(os.getcwd(), "coverage.xml"))
         num_tries += 1
 
     raise SystemExit(rc)
