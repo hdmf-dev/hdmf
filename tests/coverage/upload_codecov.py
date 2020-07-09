96f85b4faec07e6e0343015517bdff4eb49db2ba
Ryan Ly <rly@lbl.gov>
2020-07-08 17:29:45 -0700

    Fix upload script

--- tests/coverage/upload_codecov.py
+++ tests/coverage/upload_codecov.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
     max_tries = 10
     num_tries = 1
     cmd = 'codecov --required'
-    rc = call(cmd)
+    rc = call(cmd, shell=True)
     while rc != 0 and num_tries < max_tries:
         time.sleep(5)
-        rc = call(cmd)
+        rc = call(cmd, shell=True)
         num_tries += 1
 
     raise SystemExit(rc)
