ea293f2411d0d309973abf2cd0f2b084843c897b
Ryan Ly <rly@lbl.gov>
2020-12-03 18:16:37 -0800

    Restore __init__.py

--- src/hdmf/__init__.py
+++ src/hdmf/__init__.py
@@ -1,8 +1,9 @@
 from . import query  # noqa: F401
-from ._version import get_versions  # noqa: E402
 from .container import Container, Data, DataRegion
 from .utils import docval, getargs
 
+
+from ._version import get_versions  # noqa: E402
 __version__ = get_versions()['version']
 del get_versions
 
