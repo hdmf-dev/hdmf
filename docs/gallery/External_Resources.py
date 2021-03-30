0f8ea175ce5bc1cf67e10131a7cedd4127b2ad68
Oliver Ruebel <oruebel@users.noreply.github.com>
2021-03-29 17:16:38 -0700

    Suggested edits for the ExternalResources tutorial

--- docs/gallery/External_Resources.py
+++ docs/gallery/External_Resources.py
@@ -2,10 +2,15 @@
 ExternalResources
 =============================
 
-This is a user guide to interacting with the ``ExternalResources`` class.
-The ExternalResources type is experimental and is subject to change in future releases.
-If you use this type, please provide feedback to the HDMF team so that we can
-improve the structure and access of data stored with this type for your use cases.
+This is a user guide to interacting with the 
+:py:class:`~hdmf.common.resources.ExternalResources` class as part of the
+:py:module:`~hdmf.common.resources` module.
+
+.. note::
+   The ExternalResources type is experimental and is subject to change in future 
+   releases. If you use this type, please provide feedback to the HDMF team so 
+   that we can improve the structure and access of data stored with this type for
+   your use cases.
 
 """
 
@@ -14,7 +19,38 @@ improve the structure and access of data stored with this type for your use case
 # ------------------------------------------------------
 # The :py:class:`~hdmf.common.resources.ExternalResources` class provides a way
 # to organize and map user terms (keys) to multiple resources and entities
-# from the resources.
+# from the resources. A typical use case for external resources is to link data
+# stored in datasets or attributes to ontologies. For example, you may have a
+# dataset `country` storing locations. Using 
+# :py:class:`~hdmf.common.resources.ExternalResources` allows us to link the
+# country names stored in the dataset to an ontology of all countries, enabling 
+# more rigid standardization of the data and facilitating data query and 
+# introspection. 
+#
+# From a users perspective, one can think of the ``ExternalResources`` as a 
+# simple table, in which each row associates a particular ``key`` stored in a
+# particular ``object`` (i.e., Attribute or Dataset in a file) with a particular 
+# ``entity`` (e.g., a term)  of an online ``resource`` (e.g., an ontology). 
+# That is, ``(object, key)`` refer to parts inside a file and ``(resource, entity)``
+# refer to an external resource outside of the file, and ``ExternalResources`` 
+# allows us to link the two. To reduce data redundancy and improve data integrity 
+# ``ExternalResources`` stores this data internally in a collection of 
+# interlinked tables.
+# * :py:class:`~hdmf.common.resources.KeyTable` where each row describes a
+#   :py:class:`~hdmf.common.resources.Key` 
+# * :py:class:`~hdmf.common.resources.ResourceTable` where each row describes a
+#   :py:class:`~hdmf.common.resources.Resource` 
+# * :py:class:`~hdmf.common.resources.EntityTable`  where each row describes an
+#   :py:class:`~hdmf.common.resources.Entity`
+# * :py:class:`~hdmf.common.resources.ObjectTable` where each row descibes an
+#   :py:class:`~hdmf.common.resources.Object`
+# * :py:class:`~hdmf.common.resources.ObjectKeyTable` where each row describes an
+#  :py:class:`~hdmf.common.resources.ObjectKey` pair identifying which keys
+#   are used by which objects.
+#
+# The :py:class:`~hdmf.common.resources.ExternalResources` class then provides
+# convenienve functions to simply interation with these tables, allowing users
+# to treat ``ExternalResources`` as a single large table as much as possible. 
 
 ###############################################################################
 # Creating an instance of the ExternalResources class
@@ -28,18 +64,24 @@ er = ExternalResources(name='example')
 ###############################################################################
 # Using the add_ref method
 # ------------------------------------------------------
-# add_ref will be one of the ExternalResources class methods that simplifies
-# how to add new data.
-# You can think of add_ref as a wrapper function.
+# :py:func:`~hdmf.common.resources.ExternalResources.add_ref`
+# is a wrapper function provided by the ExternalResources class, that 
+# simplifies adding data. Using add_ref allows us to treat new entries similar 
+# to adding a new row to a flat table, with add_ref taking care of populating 
+# the underlying data structures accordingly.
 #
-# The function takes in the keyword arguments and will automatically create
-# new entries into tables that don't have the inputs as existing data.
-# In our example below, all of our Tables were empty before add_ref,
+# add_ref takes in a set of the keyword arguments and automatically creates
+# new entries into the approbriate tables that underly ExternalResources as
+# necessary. In our example below, all of our tables were empty before add_ref,
 # so calling the method will create new rows of data in every table
 # mentioned prior.
-# Note: If you want to reference objects then you need to reference the id
-# as a string, but if you want to reference a key then you reference
-# the key object itself.
+# 
+# .. note:: 
+#     If you want to reference objects then you need to reference the id
+#     as a string, but if you want to reference a key then you reference
+#     the :py:class:`~hdmf.common.resources.Key` object itself. We can retrive
+#     keys via the :py:func:`~hdmf.common.resources.ExternalResources.get_key`
+#     described below.
 
 data = Data(name="species", data=['Homo sapiens', 'Mus musculus'])
 er.add_ref(container=data, field='', key='Homo sapiens', resource_name='NCBI_Taxonomy',
@@ -53,12 +95,12 @@ er.add_ref(container=data, field='', key='Mus musculus', resource_name='NCBI_Tax
 ###############################################################################
 # Using the add_ref method with a field
 # ------------------------------------------------------
-# In the above example, field was empty because the data of the Data object was
-# being associated with a resource. The Data object may also have attributes
-# that you would like to associate with resources.
-# Similarly, a Container object may have several datasets and attributes that
-# you would like to associate with resources.
-# To disambiguate between these different fields, you can set the 'field' keyword
+# In the above example, ``field`` was empty because the data of the ``container`` 
+# data object itself was being associated with a resource. However, rather than
+# the ``container`` data object itself, you may instead want to associated external 
+# resources with datasets or attributes contained in the data object. 
+# that you would like to associate with resources. To disambiguate between 
+# these different fields, you can set the 'field' keyword
 # argument corresponding to the name of the dataset or attribute containing the
 # values being associated with a resource. For example:
 genotypes = DynamicTable(name='genotypes', description='My genotypes')
@@ -87,7 +129,7 @@ er.get_keys(keys=[er.get_key('Homo sapiens'), er.get_key('Mus musculus')])
 ###############################################################################
 # Using the add_key and the get_key methods
 # ------------------------------------------------------
-# Mentioned in the note aobve, the user needs to reference a key object
+# Mentioned in the note above, the user needs to reference a key object
 # when referencing a key. Let's start by creating a new key.
 
 # Let's start with a new instance of ExternalResources
@@ -136,7 +178,8 @@ er.add_keys(df)
 ###############################################################################
 # Using the add_resource method
 # ------------------------------------------------------
-# This method creates new resource objects and adds them to the ResourceTable.
+# This method creates new resource objects and adds them to the 
+# :py:class:`~hdmf.common.resources.ResourceTable`.
 # The ResourceTable holds the name and uri for the resource.
 # For example, you could add NCBI Taxonomy with its uri.
 
@@ -151,7 +194,8 @@ er.resources.to_dataframe()
 ###############################################################################
 # Using the add_entity method
 # ------------------------------------------------------
-# This method creates a new entity object and adds it to the EntityTable.
+# This method creates a new entity object and adds it to the
+# :py:class:`~hdmf.common.resources.WntityTable`.
 # Keeping with the NCBI Taxonomy example, the entity would be a specfic item
 # or "search" in NCBI Taxonomy, whereas the resource is NCBI Taxonomy itself.
 
@@ -169,7 +213,8 @@ er.entities.to_dataframe()
 ###############################################################################
 # Using the add_object method
 # ------------------------------------------------------
-# This method creates a new object and adds it to the ObjectTable.
+# This method creates a new object and adds it to the 
+# :py:class:`~hdmf.common.resources.ObjectTable`.
 
 er.add_object(container=data, field='Data')
 
@@ -179,10 +224,10 @@ er.objects.to_dataframe()
 ###############################################################################
 # Using the add_external_reference method
 # ------------------------------------------------------
-# This method creates objectkey to ObjectKeyTable
+# This method creates objectkey to 
+# :py:class:`~hdmf.common.resources.ObjectKeyTable`
 
 # Let's create a new Data instance
-
 data_mouse = Data(name="species", data='Mus musculus')
 
 # This method below returns an Object object.
@@ -190,7 +235,8 @@ object_ = er._check_object_field(data_mouse, field='')
 
 key_object = er.get_key(key_name='Mus musculus')
 
-er.add_external_reference(object_, key_object)
+# Create a new ObjectKey and add it to the ObjectKeyTable
+object_key_ = er.add_external_reference(object_, key_object)
 
-# This return ObjectKeyTable
+# This returns the ObjectKeyTable and converts it to a datafram
 er.object_keys.to_dataframe()
