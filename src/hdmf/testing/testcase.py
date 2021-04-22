90d59631d1730e8df85669272eea2a79065a3018
Ryan Ly <rly@lbl.gov>
2021-04-21 18:32:14 -0700

    Revert "Validation finds unexpected elements (#542)"
    This reverts commit 67cffecad50fbc31f8406367f9442b6e3bd87a4a.

--- src/hdmf/testing/testcase.py
+++ src/hdmf/testing/testcase.py
@@ -4,7 +4,6 @@ import os
 import re
 import unittest
 from abc import ABCMeta, abstractmethod
-from warnings import warn
 
 from .utils import remove_test_file
 from ..backends.hdf5 import HDF5IO
@@ -12,7 +11,6 @@ from ..build import Builder
 from ..common import validate as common_validate, get_manager
 from ..container import AbstractContainer, Container, Data
 from ..query import HDMFDataset
-from ..validate.errors import WARNING_SEVERITY
 
 
 class TestCase(unittest.TestCase):
@@ -227,25 +225,14 @@ class H5RoundTripMixin(metaclass=ABCMeta):
         """Validate the written and exported files, if they exist."""
         if os.path.exists(self.filename):
             with HDF5IO(self.filename, manager=get_manager(), mode='r') as io:
-                result = common_validate(io, experimental=experimental)
-                errors, warnings = self._split_validation_result(result)
+                errors = common_validate(io, experimental=experimental)
                 if errors:
-                    raise Exception("Validation Errors %s" % errors)
-                if warnings:
-                    warn("Validation Warnings: %s" % warnings)
+                    for err in errors:
+                        raise Exception(err)
 
         if os.path.exists(self.export_filename):
             with HDF5IO(self.filename, manager=get_manager(), mode='r') as io:
-                result = common_validate(io, experimental=experimental)
-                errors, warnings = self._split_validation_result(result)
+                errors = common_validate(io, experimental=experimental)
                 if errors:
-                    raise Exception("Validation Errors %s" % errors)
-                if warnings:
-                    warn("Validation Warnings: %s" % warnings)
-
-    @staticmethod
-    def _split_validation_result(result):
-        """Split validation result into errors and warnings"""
-        errors = [alert for alert in result if alert.severity > WARNING_SEVERITY]
-        warnings = [alert for alert in result if alert.severity == WARNING_SEVERITY]
-        return errors, warnings
+                    for err in errors:
+                        raise Exception(err)
