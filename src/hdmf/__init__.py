a6e530f9220119cc5e4b634deb2052b47786495e
Ryan Ly <rly@lbl.gov>
2022-03-27 12:40:49 -0700

    Test a commit that changes documentation

--- src/hdmf/__init__.py
+++ src/hdmf/__init__.py
@@ -4,22 +4,6 @@ from .utils import docval, getargs
 from .region import ListSlicer
 from .backends.hdf5.h5_utils import H5RegionSlicer, H5Dataset
 
-
-@docval({'name': 'dataset', 'type': None, 'doc': 'the HDF5 dataset to slice'},
-        {'name': 'region', 'type': None, 'doc': 'the region reference to use to slice'},
-        is_method=False)
-def get_region_slicer(**kwargs):
-    import warnings  # noqa: E402
-    warnings.warn('get_region_slicer is deprecated and will be removed in HDMF 3.0.', DeprecationWarning)
-
-    dataset, region = getargs('dataset', 'region', kwargs)
-    if isinstance(dataset, (list, tuple, Data)):
-        return ListSlicer(dataset, region)
-    elif isinstance(dataset, H5Dataset):
-        return H5RegionSlicer(dataset, region)
-    return None
-
-
 from ._version import get_versions  # noqa: E402
 __version__ = get_versions()['version']
 del get_versions
