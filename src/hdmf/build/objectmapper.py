d09cef25ac8b1fa334de4327c5efafa3984d860b
Ryan Ly <rly@lbl.gov>
2022-08-04 10:21:38 -0700

    Update src/hdmf/build/objectmapper.py
    Co-authored-by: Oliver Ruebel <oruebel@users.noreply.github.com>

--- src/hdmf/build/objectmapper.py
+++ src/hdmf/build/objectmapper.py
@@ -1256,6 +1256,8 @@ class ObjectMapper(metaclass=ExtenderMeta):
         """A wrapper function for ensuring a container gets everything set appropriately"""
         obj = cls.__new__(cls, container_source=container_source, parent=parent, object_id=object_id,
                           in_construct_mode=True)
+        # obj has been created and is in construction mode, indicating that the object is being constructed by
+        # the automatic construct process during read, rather than by the user
         obj.__init__(**kwargs)
         obj._in_construct_mode = False  # reset to False to indicate that the construction of the object is complete
         return obj
