33e7eca4fb981c023288f147dd087397d74246b4
Ryan Ly <rly@lbl.gov>
2022-09-20 16:33:24 -0700

    Add doc

--- src/hdmf/common/table.py
+++ src/hdmf/common/table.py
@@ -661,6 +661,8 @@ class DynamicTable(Container):
         Add a column to this table.
 
         If data is provided, it must contain the same number of rows as the current state of the table.
+        
+        Extra keyword arguments will be passed to the constructor of the column class ("col_cls"). 
 
         :raises ValueError: if the column has already been added to the table
         """
