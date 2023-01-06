01e7801a95052125e8c0bb983effb6518bc2d995
mavaylon1 <mavaylon1@berkeley.edu>
2023-01-06 08:08:40 -0800

    fix

--- src/hdmf/common/resources.py
+++ src/hdmf/common/resources.py
@@ -211,8 +211,6 @@ class ExternalResources(Container):
             pd.testing.assert_frame_equal(left.object_keys.to_dataframe(),
                                           right.object_keys.to_dataframe(),
                                           check_dtype=check_dtype)
-        except AssertionError as e:
-            errors.append(e)
 
         if len(errors) > 0:
             msg = ''.join(str(e)+"\n\n" for e in errors)
