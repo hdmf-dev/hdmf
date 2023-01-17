77a50ed0b524628611bcb286eedcd8bb0ca873c1
mavaylon1 <mavaylon1@berkeley.edu>
2023-01-17 09:46:37 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'HDMF'
-copyright = u'2017-2022, Hierarchical Data Modeling Framework'
+copyright = u'2017-2023, Hierarchical Data Modeling Framework'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
