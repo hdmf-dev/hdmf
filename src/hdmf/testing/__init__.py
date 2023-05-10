4f09ff4d9a3ac9e9f8e94e102e0b554a15ebfa3c
rly <rly@lbl.gov>
2023-05-10 15:25:19 -0700

    Remove unnecessary noqa

--- src/hdmf/testing/__init__.py
+++ src/hdmf/testing/__init__.py
@@ -1,2 +1,2 @@
-from .testcase import H5RoundTripMixin, TestCase  # noqa: F401
-from .utils import remove_test_file  # noqa: F401
+from .testcase import H5RoundTripMixin, TestCase
+from .utils import remove_test_file
