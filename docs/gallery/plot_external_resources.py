e696e0c40246d1a22b2f9b902a0c9be86af5fad4
Matthew Avaylon <mavaylon1@berkeley.edu>
2023-07-05 22:49:43 -0700

    Update docs/gallery/plot_external_resources.py
    Co-authored-by: Ryan Ly <rly@lbl.gov>

--- docs/gallery/plot_external_resources.py
+++ docs/gallery/plot_external_resources.py
@@ -331,7 +331,6 @@ os.remove('./er.zip')
 # :py:class:`~hdmf.common.resources.ExternalResources`. These enumerations take place of the
 # entity_id and entity_uri parameters. :py:class:`~hdmf.common.resources.Key` values will have
 # to match the name of the term in the :py:class:`~hdmf.term_set.TermSet`.
-import linkml_runtime
 from hdmf.term_set import TermSet
 
 try:
