5e584c5aa06708295d011b4fc81ee45d4623efc1
mavaylon1 <mavaylon1@berkeley.edu>
2023-07-28 11:04:47 -0400

    dynamic

--- src/hdmf/term_set.py
+++ src/hdmf/term_set.py
@@ -130,7 +130,8 @@ class TermSet():
         # TODO: should linkml raise a warning if the schema does not have dynamic enums
         enum = list(self.view.all_enums())
         schema_dir = os.path.dirname(self.term_schema_path)
-        output_path = os.path.join(schema_dir, "expanded_term_set.yaml")
+        file_name = os.path.basename(self.term_schema_path)
+        output_path = os.path.join(schema_dir, "expanded_"file_name+".yaml")
         expander.expand_in_place(self.term_schema_path, enum, output_path)
 
         return output_path
--- tests/unit/test_term_set.py
+++ tests/unit/test_term_set.py
@@ -2,7 +2,8 @@ import os
 import unittest
 
 from hdmf.term_set import TermSet
-from hdmf.testing import TestCase
+from hdmf.testing import TestCase, remove_test_file
+
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -60,3 +61,4 @@ class TestTermSet(TestCase):
     def test_enum_expander(self):
         termset = TermSet(term_schema_path='tests/unit/example_dynamic_term_set.yaml', dynamic=True)
         self.assertEqual(len(termset.view_set), 502)
+        remove_test_file('./expanded_term_set.yaml')
