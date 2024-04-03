e983aa0c1ca3015de4de830ab7fc088a4bbce572
mavaylon1 <mavaylon1@berkeley.edu>
2024-04-03 15:10:21 -0700

    typing

--- src/hdmf/build/classgenerator.py
+++ src/hdmf/build/classgenerator.py
@@ -1,7 +1,6 @@
 from copy import deepcopy
 from datetime import datetime, date
 import types
-import typing
 
 import numpy as np
 
@@ -37,7 +36,7 @@ class ClassGenerator:
             {'name': 'spec', 'type': BaseStorageSpec, 'doc': ''},
             {'name': 'parent_cls', 'type': type, 'doc': ''},
             {'name': 'attr_names', 'type': dict, 'doc': ''},
-            {'name': 'post_init_method', 'type': typing.Callable, 'default': None,
+            {'name': 'post_init_method', 'type': types.FunctionType, 'default': None,
              'doc': 'The function used as a post_init method to validate the class generation.'},
             {'name': 'type_map', 'type': 'hdmf.build.manager.TypeMap', 'doc': ''},
             returns='the class for the given namespace and data_type', rtype=type)
