398ab2dbb428170f6901bacdf5def5290edab564
rly <rly@lbl.gov>
2024-04-10 10:16:08 -0700

    stash changes

--- src/hdmf/container.py
+++ src/hdmf/container.py
@@ -937,7 +937,10 @@ class Data(AbstractContainer):
 
     def get(self, args):
         if isinstance(self.data, (tuple, list)) and isinstance(args, (tuple, list, np.ndarray)):
+            # try:
             return [self.data[i] for i in args]
+            # except:
+                # breakpoint()
         if isinstance(self.data, h5py.Dataset) and isinstance(args, np.ndarray):
             # This is needed for h5py 2.9 compatibility
             args = args.tolist()
