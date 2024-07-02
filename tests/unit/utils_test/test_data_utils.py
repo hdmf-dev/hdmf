e86ff6e61ebd4263f630a6147dcf0ab0b3d215cd
mavaylon1 <mavaylon1@berkeley.edu>
2024-07-02 15:44:43 -0700

    coverage

--- tests/unit/utils_test/test_data_utils.py
+++ tests/unit/utils_test/test_data_utils.py
@@ -3,7 +3,6 @@ from hdmf.testing import TestCase
 
 import numpy as np
 from numpy.testing import assert_array_equal
-import unittest
 
 try:
     import zarr
@@ -11,22 +10,30 @@ try:
 except ImportError:
     ZARR_INSTALLED = False
 
+
+class MyIterable:
+    def __init__(self, data):
+        self.data = data
+
+
 class TestAppendData(TestCase):
+    def test_append_exception(self):
+        data = MyIterable([1, 2, 3, 4, 5])
+        with self.assertRaises(ValueError):
+            rappend_data(data, 4)
 
-    def test_append_data_unknown_error(self):
-        obj = object()
-        msg = "Data cannot append to object of type '<class 'object'>'"
-        with self.assertRaisesWith(ValueError, msg):
-            append_data(obj, 4)
 
-    @unittest.skipIf(ZARR_INSTALLED, "optional Zarr package is installed")
-    def test_append_data_zarr_error(self):
-        zarr_array = zarr.array([1,2,3])
-        msg = "Data cannot append to object of type '<class 'zarr.core.Array'>'"
-        with self.assertRaisesWith(ValueError, msg):
-            append_data(zarr_array, 4)
+class TestZarrAppendData(TestCase):
 
-    @unittest.skipIf(not ZARR_INSTALLED, "optional Zarr package is not installed")
+    def setUp(self):
+        if not ZARR_INSTALLED:
+            self.skipTest("optional Zarr package is not installed")
+
+    def test_append_exception(self):
+        data = MyIterable([1, 2, 3, 4, 5])
+        with self.assertRaises(ValueError):
+            rappend_data(data, 4)
+
     def test_append_data_zarr(self):
         zarr_array = zarr.array([1,2,3])
         new = append_data(zarr_array, 4)
