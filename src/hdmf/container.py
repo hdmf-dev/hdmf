1d40cca8efb6a54c8dd1fe1770a6b0a4c0c6b986
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-08-15 11:38:13 -0700

    reorganized fn signature

--- src/hdmf/container.py
+++ src/hdmf/container.py
@@ -830,9 +830,13 @@ class Container(AbstractContainer):
         out += '\n' + indent + right_br
         return out
 
-    def set_data_io(self, dataset_name: str, data_io_class: Type[DataIO], data_io_kwargs: dict = None,
-                    data_chunk_iterator_class: Type[AbstractDataChunkIterator] = DataChunkIterator,
-                    data_chunk_iterator_kwargs: dict = None, **kwargs):
+    def set_data_io(
+        self,
+        dataset_name: str,
+        data_io_class: Type[DataIO], data_io_kwargs: dict = None,
+        data_chunk_iterator_class: Type[AbstractDataChunkIterator] = DataChunkIterator,
+        data_chunk_iterator_kwargs: dict = None, **kwargs
+    ):
         """
         Apply DataIO object to a dataset field of the Container.
 
