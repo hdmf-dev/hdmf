4cd1d817628ce364d15fe74639ecdfdd0f2120da
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-10-16 14:44:55 -0700

    remove deprecated call to get_html_theme_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -163,15 +163,11 @@ pygments_style = "sphinx"
 # html_theme = 'default'
 # html_theme = "sphinxdoc"
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 # html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
