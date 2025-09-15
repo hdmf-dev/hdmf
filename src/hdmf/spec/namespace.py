0f08d29543a8f85bc2ec1bed5ccc3496834fc772
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-09-15 11:16:51 -0700

    fix formatting

--- src/hdmf/spec/namespace.py
+++ src/hdmf/spec/namespace.py
@@ -705,7 +705,10 @@ class NamespaceCatalog:
                 if core_parent_type:
                     core_namespace = self.__namespaces[core_namespace_name]
                     core_spec = core_namespace.get_spec(core_parent_type)
-                    conflict_msg = self._check_cross_type_conflicts(extension_ns_name, type_name, extension_spec, core_spec)
+                    conflict_msg = self._check_cross_type_conflicts(extension_ns_name, 
+                                                                    type_name, 
+                                                                    extension_spec, 
+                                                                    core_spec)
                     if conflict_msg is not None:
                         warning_msg.append(conflict_msg)
 
@@ -734,8 +737,9 @@ class NamespaceCatalog:
                         break
 
                 if core_link is not None:
-                    warning_msg = (f"{extension_ns_name} defines {spec_name}.{attr_spec.name} as an attribute (dtype: {attr_spec.dtype}) "
-                                   f"while the core schema defines it as a link to {core_link.target_type}. ")
+                    warning_msg = (f"{extension_ns_name} defines {spec_name}.{attr_spec.name} as an "
+                                   f"attribute (dtype: {attr_spec.dtype}) while the core schema defines "
+                                   f"it as a link to {core_link.target_type}.")
                     return warning_msg
 
         # Check all links in extension spec against links in core spec for target_type conflicts
@@ -749,9 +753,12 @@ class NamespaceCatalog:
 
                 if (core_link_spec is not None
                     and core_link_spec.target_type != ext_link_spec.target_type
-                    and not self.is_sub_data_type(extension_ns_name, ext_link_spec.target_type, core_link_spec.target_type)):
-                    warning_msg = (f"{extension_ns_name} defines {spec_name}.{ext_link_spec.name} as a link to {ext_link_spec.target_type} "
-                                   f"while the core schema defines it as a link to {core_link_spec.target_type}. {ext_link_spec.target_type} "
+                    and not self.is_sub_data_type(extension_ns_name, 
+                                                  ext_link_spec.target_type, 
+                                                  core_link_spec.target_type)):
+                    warning_msg = (f"{extension_ns_name} defines {spec_name}.{ext_link_spec.name} as a "
+                                   f"link to {ext_link_spec.target_type} while the core schema defines it as "
+                                   f"a link to {core_link_spec.target_type}. {ext_link_spec.target_type} "
                                    f"is not a subtype of {core_link_spec.target_type}. ")
                     return warning_msg
 
