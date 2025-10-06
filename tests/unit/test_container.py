2ff823b6e6b2b285217e6cf7b203cb15e63335c9
rly <rly@lbl.gov>
2025-10-06 10:45:05 -0700

    Remove HERDManager test

--- tests/unit/test_container.py
+++ tests/unit/test_container.py
@@ -25,6 +25,7 @@ class ContainerWithChild(Container):
 
 
 class TestHERDManager(TestCase):
+
     def test_get_and_set_resources(self):
         em = HERDManager()
         er = HERD()
@@ -34,21 +35,6 @@ class TestHERDManager(TestCase):
 
         er_get = em.external_resources
         self.assertEqual(er, er_get)
-
-    def test_link_resources_deprecated(self):
-        em = HERDManager()
-        er = HERD()
-        with self.assertWarns(DeprecationWarning):
-            em.link_resources(herd=er)
-        self.assertEqual(em.external_resources, er)
-
-    def test_get_linked_resources_deprecated(self):
-        em = HERDManager()
-        er = HERD()
-        em.external_resources = er
-        with self.assertWarns(DeprecationWarning):
-            herd = em.get_linked_resources()
-        self.assertEqual(herd, er)
 
 
 class TestContainer(TestCase):
