9b2b573fae95d6ae364f7210db539623a8b9b3f9
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-11-30 22:04:40 -0800

    fix line length

--- tests/unit/validator_tests/test_validate.py
+++ tests/unit/validator_tests/test_validate.py
@@ -1649,7 +1649,9 @@ class TestObjectDtypeArrays(TestCase):
         self.set_up_spec()
 
         # Create a zarr array with object dtype containing bytes (ASCII strings)
-        zarr_array = zarr.array(np.array(['string1', 'string2'], dtype=bytes), dtype=object, object_codec=numcodecs.VLenBytes())
+        zarr_array = zarr.array(np.array(['string1', 'string2'], dtype=bytes), 
+                                dtype=object, 
+                                object_codec=numcodecs.VLenBytes())
         bar_builder = GroupBuilder('my_bar',
                                    attributes={'data_type': 'Bar', 'attr1': 'a string attribute'},
                                    datasets=[DatasetBuilder('data', zarr_array)])
